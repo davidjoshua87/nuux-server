a3e3960c8cd80d92c750abe357373c1291286d91
davidjoshua87 <davidjosh_87@yahoo.com>
2022-11-01 04:04:05 +0700

    fix: #update app.js

--- app.js
+++ app.js
@@ -37,6 +37,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/api/auth/", authRouter);
 app.use("/api/", indexRouter);
 
